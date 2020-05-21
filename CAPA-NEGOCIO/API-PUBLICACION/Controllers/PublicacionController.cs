500edbf326d64fe945da54f9894f95955603dc5a
EmmaGrande <emajulio.ej@gmail.com>
2020-05-20 22:24:50 -0300

    A mitad de camino de arreglar los metodos post de apipublicacion

--- CAPA-NEGOCIO/API-PUBLICACION/Controllers/PublicacionController.cs
+++ CAPA-NEGOCIO/API-PUBLICACION/Controllers/PublicacionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CAPA_APLICACION.SERVICIOS;
+using CAPA_NEGOCIO.ENTIDADES;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +18,20 @@ namespace API_PUBLICACION.Controllers
         public PublicacionController(IPublicacionService service)
         {
             this.service = service;
+        }
+        [HttpPost]
+        public IActionResult Post(PublicacionDTO comentario)
+        {
+            try
+            {
+                return new JsonResult(service.CrearPublicacion(comentario)) { StatusCode = 201 };
+
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+
+            }
         }
     }
 }
